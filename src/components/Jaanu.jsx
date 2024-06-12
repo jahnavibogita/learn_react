f9d7c0a50749875530c64b62ee45bfabd225fee7
jahnavibogita <bogitajahnavi11@gmail.com>
2024-06-12 13:20:33 +0530

    functional and class components,structure

---
+++ src/components/Jaanu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Jaanu = () => {
+    return (
+        <h1>jaanu is reading</h1>
+    )
+    
+}
+// function Jaanu() {
+//     return (
+//         <div>
+//             <h1>Jaanu is learning react</h1>
+//         </div>
+//     )
+// }
+export default Jaanu;
