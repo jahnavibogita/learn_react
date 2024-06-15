328f9973b0678201884d39dcbcd62c498b4bae30
jahnavibogita <bogitajahnavi11@gmail.com>
2024-06-15 14:08:32 +0530

    learnt functonal class components and props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Jaanu from './components/Jaanu'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 function App() {
   return (
     <div className="App">
-      <Welcome/>
-      <Jaanu/>
+      {/* <Welcome/>
+      <Jaanu /> */}
+      <Hello name="jahnavi" heroname="itachi"/>
+      <Hello name="bhavs" />
+      <Hello name="scooty"><p>hey man!!</p></Hello>
+      <Welcome name="jaanu"><p>works hard!</p></Welcome>
+      
     </div>
   );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,10 +3,11 @@ class Welcome extends Component{
     render() {
         return (
             <div>
-                <h1>welcome all!</h1>
+                <h1>welcome all!{this.props.name}</h1>
+                {this.props.children}
             </div>
         )
     }
 
 }
-export default Welcome
+export default Welcome 
